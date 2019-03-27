c32ee97fcdb9f06a7ef8704d09aec38c996a9207
DarkLORDCOSMOS <jsamson3@msudenver.edu>
2019-03-27 15:29:20 -0600

    refactoring

--- src/Project_25/Floyds.java
+++ src/Project_25/Floyds.java
@@ -1,3 +1,5 @@
+package Project_25;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
--- src/Project_4/SumsToN.java
+++ src/Project_4/SumsToN.java
@@ -1,3 +1,5 @@
+package Project_4;
+
 import java.util.Scanner;
 
 public class SumsToN {
