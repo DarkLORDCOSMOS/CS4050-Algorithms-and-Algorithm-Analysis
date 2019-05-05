ca875660c62875362d48a585e1cbd2f48673c6d5
DarkLORDCOSMOS <jsamson3@msudenver.edu>
2019-05-05 17:28:50 -0600

    Updated Project 32

--- src/Project_32/p32.java
+++ src/Project_32/p32.java
@@ -6,15 +6,27 @@ import java.io.*;
 public class p32 {
 
     public static void main(String[] args) throws IOException {
-        double cx, cy, vx, vy, dx, dy, wx, wy, rows, columns;
+        double cx, cy, vx, vy, dx, dy, wx, wy;
+        int a1x, a1y, a2x, a2y, a3x, a3y, b1x, b1y, b2x, b2y, b3x, b3y, b4x, b4y;
+        String[] headers = {"la", "mu1", "mu2", "mu3", "v1", "v2", "v3", "v4", "rhs"};
+        int rows, columns;
+        // String[] headers;
         Scanner input = new Scanner( System.in );
 
         final String OUTPUT_FILE = "p32.txt";
         FileWriter outputDataFile = new FileWriter(OUTPUT_FILE);
         PrintWriter outputFile = new PrintWriter(outputDataFile);
 
-        rows = 4;
-        columns = 19;
+        a1x = 1; a1y = -1;
+        a2x = 2; a2y = 1;
+        a3x = -1; a3y = 3;
+        b1x = -1; b1y = -1;
+        b2x = 2; b2y = 0;
+        b3x = -1; b3y = 1;
+        b4x = -3; b4y = 0;
+
+        rows = 5;
+        columns = headers.length;
 
         System.out.print("Enter x for first object center point, c: ");
         cx = input.nextDouble();
@@ -36,14 +48,150 @@ public class p32 {
 
         outputFile.println(rows + " " + columns);
 
-        outputFile.println("cx = " + cx);
-        outputFile.println("cy = " + cy);
-        outputFile.println("vx = " + vx);
-        outputFile.println("vy = " + vy);
-        outputFile.println("dx = " + dx);
-        outputFile.println("dy = " + dy);
-        outputFile.println("wx = " + wx);
-        outputFile.println("wy = " + wy);
+        for (int i = 1; i < rows; i ++) {
+            if (i == 1) {
+                outputFile.println("la");
+            }
+            outputFile.println("-");
+        }
+
+        for (String header: headers) {
+            outputFile.println(header);
+        }
+
+        //        int j = 0; j < columns; j++) {
+//            if (j == columns - 1) {
+//                outputFile.println(dx + dy - cx - cy);
+//            }
+////            outputFile.println("-");
+//        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (i == 0) {
+                    if (j == 0) {
+                        outputFile.print(1 + " ");
+                    }
+                }
+                if (i == 1) {
+                    if (j == 0) {
+                        outputFile.print(((int) (vx - wx)) + " ");
+                    }
+                    else if (j == 1) {
+                        outputFile.print(a1x + " ");
+                    }
+                    else if (j == 2) {
+                        outputFile.print(a2x + " ");
+                    }
+                    else if (j == 3) {
+                        outputFile.print(a3x + " ");
+                    }
+                    else if (j == 4) {
+                        outputFile.print(b1x + " ");
+                    }
+                    else if (j == 5) {
+                        outputFile.print(b2x + " ");
+                    }
+                    else if (j == 6) {
+                        outputFile.print(b3x + " ");
+                    }
+                    else if (j == 7) {
+                        outputFile.print(b4x + " ");
+                    }
+                    else if (j == columns - 1) {
+                        outputFile.print(((int) (dx - cx)) + " ");
+                    }
+                    else {
+                        outputFile.print("0 ");
+                    }
+                }
+                else if (i == 2) {
+                    if (j == 0) {
+                        outputFile.print(((int) (vy - wy)) + " ");
+                    }
+                    else if (j == 1) {
+                        outputFile.print(a1y + " ");
+                    }
+                    else if (j == 2) {
+                        outputFile.print(a2y + " ");
+                    }
+                    else if (j == 3) {
+                        outputFile.print(a3y + " ");
+                    }
+                    else if (j == 4) {
+                        outputFile.print(b1y + " ");
+                    }
+                    else if (j == 5) {
+                        outputFile.print(b2y + " ");
+                    }
+                    else if (j == 6) {
+                        outputFile.print(b3y + " ");
+                    }
+                    else if (j == 7) {
+                        outputFile.print(b4y + " ");
+                    }
+                    else if (j == columns - 1) {
+                        outputFile.print(((int) (dy - cy)) + " ");
+                    }
+                    else {
+                        outputFile.print("0 ");
+                    }
+                }
+                else if (i == 3) {
+                    if (j == 1) {
+                        outputFile.print(1 + " ");
+                    }
+                    else if (j == 2) {
+                        outputFile.print(1 + " ");
+                    }
+                    else if (j == 3) {
+                        outputFile.print(1 + " ");
+                    }
+                    else if (j == columns - 1) {
+                        outputFile.print(1 + " ");
+                    }
+                    else {
+                        outputFile.print("0 ");
+                    }
+                }
+                else if (i == 4) {
+                    if (j == 4) {
+                        outputFile.print(1 + " ");
+                    }
+                    else if (j == 5) {
+                        outputFile.print(1 + " ");
+                    }
+                    else if (j == 6) {
+                        outputFile.print(1 + " ");
+                    }
+                    else if (j == 7) {
+                        outputFile.print(1 + " ");
+                    }
+                    else if (j == columns - 1) {
+                        outputFile.print(1 + " ");
+                    }
+                    else {
+                        outputFile.print("0 ");
+                    }
+                }
+                else {
+                    outputFile.print("0 ");
+                }
+            }
+            if (i == 2) {
+
+            }
+            outputFile.println();
+        }
+
+//        outputFile.println("cx = " + cx);
+//        outputFile.println("cy = " + cy);
+//        outputFile.println("vx = " + vx);
+//        outputFile.println("vy = " + vy);
+//        outputFile.println("dx = " + dx);
+//        outputFile.println("dy = " + dy);
+//        outputFile.println("wx = " + wx);
+//        outputFile.println("wy = " + wy);
 
         outputFile.close();
         System.exit(0);
