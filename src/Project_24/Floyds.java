d313578a68cb7b319d2acd71cba03c71f25d0778
DarkLORDCOSMOS <jsamson3@msudenver.edu>
2019-05-15 21:03:04 -0600

    Updated Project 24

--- src/Project_24/Floyds.java
+++ src/Project_24/Floyds.java
@@ -11,7 +11,15 @@ public class Floyds {
 
     public static void main(String[] args) throws FileNotFoundException {
         int[][] d0 = input(args[0]);
-        floyds(d0, 0);
+        int[][] v0 = new int [numVertices][numVertices];
+
+        for (int i = 0; i < numVertices; i++){
+            for (int j = 0; j < numVertices; j++){
+                v0[i][j] = 0;
+            }
+        }
+
+        floyds(d0, v0, 0);
     }
 
     private static int[][] input(String inputFile) throws FileNotFoundException {
@@ -26,25 +34,26 @@ public class Floyds {
         return d0;
     }
 
-    private static void floyds(int[][] chart, int count) {
-        if (count == 0) { print(count++, chart); }
-        chart = getShortestPath(chart, count);
-        print(count, chart);
+    private static void floyds(int[][] chart, int[][] vertices, int count) {
+        if (count == 0) { print(count++, chart, vertices); }
+        chart = getShortestPath(chart, vertices, count);
+        print(count, chart, vertices);
         if (count == numVertices) {
             return;
         }
         else {
-            floyds(chart, count + 1);
+            floyds(chart, vertices,count + 1);
         }
     }
 
-    private static int[][] getShortestPath(int[][] chart, int count) {
+    private static int[][] getShortestPath(int[][] chart, int[][] vertices, int count) {
         for (int k = 0; k < count; k++) {
             for (int i = 0; i < numVertices; i++) {
                 for(int j = 0; j < numVertices; j++) {
                     if (chart[i][j] == -1) {
                         if ((chart[i][k] != -1) && (chart[k][j] != -1)) {
                             chart[i][j] = chart[i][k] + chart[k][j];
+                            vertices[i][j] = k + 1;
                         }
                     }
                     else if (chart[i][k] == -1 || chart[k][j] == -1) {
@@ -52,6 +61,7 @@ public class Floyds {
                     }
                     else if (chart[i][k] + chart[k][j] < chart[i][j]) {
                         chart[i][j] = chart[i][k] + chart[k][j];
+                        vertices[i][j] = k + 1;
                     }
                 }
             }
@@ -59,11 +69,11 @@ public class Floyds {
         return chart;
     }
 
-    private static void print(int count, int[][] chart) {
+    private static void print(int count, int[][] chart, int[][] vertices) {
         System.out.println(count);
         for (int i = 0; i < numVertices; i++) {
             for(int j = 0; j < numVertices; j++) {
-                System.out.print(chart[i][j] + " ");
+                System.out.print(chart[i][j] + "." + vertices[i][j] + " ");
             }
             System.out.println();
         }
